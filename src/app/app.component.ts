bd37ef1160dd983af3ca72c2dcef1c9e82987927
matthew-sbrito <matheusbr032@gmail.com>
2022-04-21 20:32:03 -0300

    feat: first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ColorPickerService} from "./color-picker/color-picker.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'color-picker';
+
+  constructor(
+    private colorPicker: ColorPickerService
+  ) { }
+
+  openColorPicker() {
+    this.colorPicker.open();
+  }
+
+  ngOnInit(): void {
+    console.log(this.colorPicker)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ColorPickerModule} from "./color-picker/color-picker.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+import {ColorPickerService} from "./color-picker/color-picker.service";
+import {MatDialogModule} from "@angular/material/dialog";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ColorPickerModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDialogModule
   ],
-  providers: [],
+  providers: [
+    ColorPickerService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
